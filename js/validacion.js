535b13aab027784b71f5eceec4efebf50ce4f7ab
enzulian <enzolopez837@gmail.com>
2025-08-13 14:05:47 -0300

    commit1

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,31 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+    const  alert = document.getElementById("alert-success");
+    alert.classList.add("show");
 }
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+function showAlertError(mensaje) {
+  const alert = document.getElementById("alert-danger");
+  alert.querySelector("p").textContent = mensaje;
+  alert.classList.add("show");
 }
+
+document.getElementById("regBtn").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value.trim();
+  const apellido = document.getElementById("apellido").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password1 = document.getElementById("password1").value.trim();
+  const password2 = document.getElementById("password2").value.trim();
+  const terminos = document.getElementById("terminos").checked;
+
+if (!nombre || !apellido || !email || !password1 || !password2 || !terminos) {
+    showAlertError("⚠️ Todos los campos son obligatorios y debe aceptar los términos.");
+    return;
+}
+
+if (password1.length < 6) {
+    showAlertError("⚠️ La contraseña debe tener al menos 6 caracteres.");
+    return;
+}
+
+showAlertSuccess();
+});
